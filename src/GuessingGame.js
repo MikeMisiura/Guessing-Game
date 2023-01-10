948e01ae34c430d28df489379bf10b59b94b926a
Mike Misiura <mikemisiura@gmail.com>
2023-01-09 22:01:28 -0700

    minimum completed. added all functionality, and cleaned code

--- src/GuessingGame.js
+++ src/GuessingGame.js
@@ -1,45 +1,94 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import usePersistence from './usePersistence';
+import styles from './GuessingGame.module.css';
+
 
 function GuessingGame() {
 
-    const [totGuesses, setTotGuesses] = useState(0)
-    const [luckyNunber, setluckyNunber] = useState(0)
+    const [totGuesses, setTotGuesses] = usePersistence('totGuesses', 0)
+    const [luckyNumber, setluckyNumber] = usePersistence('luckyNumber', 0)
     const [guess, setGuess] = useState(0)
+    const [guessText, setGuessText] = useState("")
+
+    useEffect(() => {
+        if (luckyNumber === 0) {
+            newLuckyNumber()
+        }
+    })
+
+    useEffect(() => {
+        if (guess === 0) {
+            setGuessText(<p>Start Guessing!</p>)
+        } else {
+            if (guess === luckyNumber) {
+                setGuessText(<p className={styles.win}>Congrats you guessed it in {totGuesses} tries!</p>)
+
+            }
+            else {
+
+                if (guess > luckyNumber) {
+                    setGuessText(<p>Guess lower than {guess}</p>)
+                }
+                else {
+
+                    if (guess < luckyNumber) {
+                        setGuessText(<p>Guess higher than {guess}</p>)
+                    }
+                }
+            }
+        }
+    }, [guess, luckyNumber, totGuesses])
 
     function newLuckyNumber() {
-        setluckyNunber(Math.floor(Math.random() * (100 - 1) + 1))
+        setluckyNumber(Math.floor(Math.random() * (101 - 1) + 1))
     }
 
     function handleReset() {
-        newLuckyNumber()
         setTotGuesses(0)
+        setGuess(0)
+        newLuckyNumber()
+        setGuessText("")
     }
 
-    function handleGuess() {
+    function handleGuess(event) {
+        event.preventDefault()
+        setGuess(parseInt(event.target.guess.value))
+
         setTotGuesses(totGuesses + 1)
     }
 
-    function getRandomInt(min, max) {
-        return; // The maximum is exclusive and the minimum is inclusive
+    function guessForm() {
+        return (
+            <Form onSubmit={handleGuess}>
+                <Form.Group className="mb-3">
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter Guess"
+                        name="guess"
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit" >{' '}
+                    Guess
+                </Button>
+
+            </Form>
+
+        )
     }
 
     return (
         <div style={{ textAlign: 'center' }}>
             <p>I am thinking of a number between 1 and 100. Guess the Lucky Number!</p>
             <p>You have made {totGuesses} guesses.</p>
-            <p>The Lucky Number is: {luckyNunber}</p>
-            <Button variant="primary" onClick={handleGuess} >{' '}
-                Guess
-            </Button>
-            <br />
+            {guessForm()}
             <Button variant="primary" onClick={handleReset} >{' '}
                 Reset
             </Button>
-
+            {guessText}
         </div>
     );
 }
-
 
 export default GuessingGame;
