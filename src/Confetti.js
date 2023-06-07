4d9687a3069ad06441fe8d0f66dd165a62cc48be
Mike Misiura <mikemisiura@gmail.com>
2023-06-07 11:01:51 -0600

    confetti and CSS

---
+++ src/Confetti.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import ReactConfetti from 'react-confetti';
+
+const Confetti = () => {
+    const [windowDimension, setDimension] = useState({
+        width: window.innerWidth, 
+        height: window.innerHeight
+    });
+
+    const detectSize = () => {
+        setDimension({width: window.innerWidth, height: window.innerHeight})
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', detectSize)
+        return () => {
+            window.addEventListener('resize', detectSize)
+        }
+    }, [windowDimension]);
+
+    return (
+        <>
+            <ReactConfetti
+                width={windowDimension.width}
+                height={windowDimension.height}
+                tweenDuration={1000}
+            />
+        </>
+    )
+}
+
+export default Confetti;
--- src/GuessingGame.js
+++ src/GuessingGame.js
@@ -3,14 +3,14 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 import usePersistence from './usePersistence';
 import styles from './GuessingGame.module.css';
-
+import Confetti from './Confetti';
 
 function GuessingGame() {
-
     const [totGuesses, setTotGuesses] = usePersistence('totGuesses', 0)
     const [luckyNumber, setluckyNumber] = usePersistence('luckyNumber', 0)
-    const [guess, setGuess] = useState(0)
+    const [guess, setGuess] = usePersistence('guess', 0)
     const [guessText, setGuessText] = useState("")
+    const [win, setWin] = useState(false)
 
     useEffect(() => {
         if (luckyNumber === 0) {
@@ -23,8 +23,8 @@ function GuessingGame() {
             setGuessText(<p>Start Guessing!</p>)
         } else {
             if (guess === luckyNumber) {
-                setGuessText(<p className={styles.win}>Congrats you guessed it in {totGuesses} tries!</p>)
-
+                setGuessText(<p className={styles.win}>Congrats! You guessed it in {totGuesses} tries!</p>)
+                setWin(true)
             }
             else {
 
@@ -50,43 +50,50 @@ function GuessingGame() {
         setGuess(0)
         newLuckyNumber()
         setGuessText("")
+        setWin(false)
     }
 
     function handleGuess(event) {
         event.preventDefault()
         setGuess(parseInt(event.target.guess.value))
-
         setTotGuesses(totGuesses + 1)
     }
 
     function guessForm() {
         return (
-            <Form onSubmit={handleGuess}>
+            <Form className={styles.form} onSubmit={handleGuess}>
                 <Form.Group className="mb-3">
                     <Form.Control
                         type="text"
                         placeholder="Enter Guess"
                         name="guess"
+                        id={styles.inputBox}
                     />
                 </Form.Group>
-                <Button variant="primary" type="submit" >{' '}
+                <Button className={styles.btn} variant="primary" type="submit" >{' '}
                     Guess
                 </Button>
-
             </Form>
-
         )
+    }
+
+    function confetti() {
+        if (win) {
+            return (<Confetti />)
+        } 
+        return
     }
 
     return (
         <div style={{ textAlign: 'center' }}>
-            <p>I am thinking of a number between 1 and 100. Guess the Lucky Number!</p>
+            <p className={styles.think}>I am thinking of a number between 1 and 100. Guess the Lucky Number!</p>
             <p>You have made {totGuesses} guesses.</p>
-            {guessForm()}
-            <Button variant="primary" onClick={handleReset} >{' '}
+            <div className={styles.inputContainer}>{guessForm()}</div>
+            {guessText}
+            <Button className={styles.btn} variant="primary" onClick={handleReset} >{' '}
                 Reset
             </Button>
-            {guessText}
+            {confetti()}
         </div>
     );
 }
